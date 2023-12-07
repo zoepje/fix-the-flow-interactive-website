cd12113c75076c6c33fa46e9a0f675d8da6aedbd
zoepje <zoe.penner@hva.nl>
2023-12-07 11:57:19 +0100

    filter

--- js.js
+++ js.js
@@ -1,11 +1,20 @@
 var filter = document.querySelector("legend");
 var labels = document.querySelector("div.labels");
+var main = document.querySelector("main");
 
 function toggleFilter() {
   labels.classList.toggle("show");
 }
 
+function closeFilter(){
+  if (labels.classList.contains("show")){
+    labels.classList.remove("show");
+  }
+}
+
 filter.addEventListener("click", toggleFilter);
+
+main.addEventListener("click", closeFilter);
 
 
 
