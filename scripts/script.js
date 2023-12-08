e6c81aea05fd40cfd10ec8d43d7cd2f324a5a4b3
zoepje <zoe.penner@hva.nl>
2023-12-08 11:49:40 +0100

    comments toevoegen #9

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,23 @@
+// Selecteren van elementen en ze een naam geven
 const legend = document.querySelector("legend");
 const labels = document.querySelector("div.labels");
 const main = document.querySelector("main");
 
+
+// Fuctie om de class "show" aan een lables te togglen(aan/uit)
 function toggleFilter() {
   labels.classList.toggle("show");
 }
 
+// Functie om de class "show" te deleten van labels als hij er op zit
 function closeFilter(){
   if (labels.classList.contains("show")){
     labels.classList.remove("show");
   }
 }
 
+
+// als je klikt op dit element dan voer deze funtie uit.
 legend.addEventListener("click", toggleFilter);
 
 main.addEventListener("click", closeFilter);
