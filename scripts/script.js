219c8a0a7ecd7119a54b42adeca89341ac46e35b
zoepje <zoe.penner@hva.nl>
2023-12-15 11:13:27 +0100

    filter edit

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,9 @@ const legend = document.querySelector("legend");
 const labels = document.querySelector("div.labels");
 const main = document.querySelector("main");
 
+const filterbtn = document.querySelector("#openMenu");
+const filter = document.querySelector("form.filter");
+
 
 // Fuctie om de class "show" aan een lables te togglen(aan/uit)
 function toggleFilter() {
@@ -16,12 +19,15 @@ function closeFilter(){
   }
 }
 
-
 // als je klikt op dit element dan voer deze funtie uit.
 legend.addEventListener("click", toggleFilter);
 
 main.addEventListener("click", closeFilter);
 
 // Filter min-width: 750px //
+// function openFilter(){
+//   filter.classList.toggle("openFilter");
+// }
 
+// filterbtn.addEventListener("click", openFilter);
 
